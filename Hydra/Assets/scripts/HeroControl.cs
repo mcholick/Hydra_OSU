eeff81d6fd25088c22e71fcc5172d64a6b3184e3
markovid <markovid@oregonstate.edu>
2018-01-25 20:10:49 -0800

    Added Character Movement/Sprites
    Wrote movement script, added sprites, animations still need linked

---
+++ Hydra/Assets/scripts/HeroControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroControl : MonoBehaviour {
+
+    public float speed; //controls hero's pace
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float adjustedSpeed = speed;  //used to make sure player can't go faster diagonally
+        //vertical movement
+        if (Input.GetAxisRaw("Vertical") != 0) {
+            if(Input.GetAxisRaw("Horizontal") != 0)
+                adjustedSpeed = Mathf.Sqrt(speed);
+            transform.Translate(0, adjustedSpeed*Input.GetAxisRaw("Vertical")/100,0);
+        }
+        //horizontal movement
+        if (Input.GetAxisRaw("Horizontal") != 0) {
+            if (Input.GetAxisRaw("Vertical") != 0)
+                adjustedSpeed = Mathf.Sqrt(speed);
+            transform.Translate(adjustedSpeed*Input.GetAxisRaw("Horizontal") / 100, 0, 0);
+        }
+	}
+}
