a48e042bf2fc94e148dfa3fcbb4e1318f3733ee4
markovid <markovid@oregonstate.edu>
2018-02-04 14:12:44 -0800

    Created Inventory/other explore phase dev
    created inventory, player can no longer walk through other characters, collects items, and stores in inventory

---
+++ Hydra/Assets/scripts/CamControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour {
+
+    public GameObject focus;  //gameobject the camera will follow
+    public float speed;
+    private Vector3 position;  //need 3rd dim to have cam sit above player
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        position = new Vector3(focus.transform.position.x, focus.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, position, speed*Time.deltaTime);
+	}
+}
--- Hydra/Assets/scripts/HeroControl.cs
+++ Hydra/Assets/scripts/HeroControl.cs
@@ -1,30 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeroControl : MonoBehaviour {
 
     public float speed; //controls hero's pace
 
-    private List<System.String> Inventory;//holds hero's stuff
+    private string[] Inventory = new string[10];//holds hero's stuff
+    int invCount = 0;
 
     private CircleCollider2D hitbox;  //used for collecting and interacting with close objects
 
     private Animator anim;
 
-    private Rigidbody2D rigbod;
+    //private Rigidbody2D rigbod;
+
+    private string invText = "Inventory:"; //display inventory on screen;
 
     // Use this for initialization
     void Start() {
         anim = GetComponent<Animator>();
-        rigbod = GetComponent<Rigidbody2D>();
+        //rigbod = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
         float moveHori = Input.GetAxisRaw("Horizontal");
         float moveVert = Input.GetAxisRaw("Vertical");
-        Vector2 movement = new Vector2(moveHori, moveVert);
         transform.Translate(new Vector2(moveHori, moveVert) * speed * Time.deltaTime);
         
         
@@ -62,11 +65,30 @@ public class HeroControl : MonoBehaviour {
         anim.SetFloat("MovingY", Input.GetAxisRaw("Vertical"));
     }
 
-    void OnTriggerEnter2D(Collider2D other){
-        if (Input.GetKeyDown("space")) {
+    void OnCollisionEnter2D(Collision2D other){
+        string keyname;
+        
+
+        if (other.gameObject.CompareTag("key")) {
+            keyname = other.gameObject.GetComponent<Text>().text;
             print("Collectible Activated");
+            //print(keyname);
+            invText = invText + "\n" + keyname;
+            Inventory[invCount] = keyname;
+            invCount++;
+            print("Inventory contains:");
+            for(int i = 0; i< invCount; i++)
+                print(Inventory[i]);
+            
+            Destroy(other.gameObject);
 
         }
+    }
+
+    private void OnGUI(){
+        Rect pos = new Rect(10, 10, 200, 3000);
+        GUI.Label(pos, invText);
+
     }
 
 }
