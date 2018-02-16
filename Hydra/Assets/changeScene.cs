15a3f546eefd69607b3adb04a9fc0b6101644c2e
Ethan Dunham <ethan.dunham@gmail.com>
2018-02-16 12:49:12 -0500

    added scene change to bosses on main map

---
+++ Hydra/Assets/changeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+         SceneManager.LoadScene("_BattleScene");
+        //Destroy(self.gameObject);
+    }
+
+}
