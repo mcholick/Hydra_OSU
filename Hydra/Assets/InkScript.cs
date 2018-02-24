6a20d0f3bfdd3600f3ed899fc4f255d9be6017ad
markovid <markovid@oregonstate.edu>
2018-02-24 10:39:57 -0800

    working on anim

---
+++ Hydra/Assets/InkScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InkScript : MonoBehaviour {
+
+    private BoxCollider2D box;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("hero")) {
+            print("Scene Transition");
+        }
+    }
+}
